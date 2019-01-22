8cee74b358d326442f60ae32621dc2f8d8789c7c
Phil Marcoccia <36013742+frostie@users.noreply.github.com>
2019-01-21 20:22:53 -0500

    Add files via upload

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -8,7 +8,6 @@ public class Block : MonoBehaviour {
     // Configuration parameters
     [SerializeField] AudioClip breakSound;
     [SerializeField] GameObject blockSparklesVFX;
-    [SerializeField] int maxHits = 2;
     [SerializeField] Sprite[] hitSprites;
 
     // Cached references
@@ -43,12 +42,20 @@ public class Block : MonoBehaviour {
     private void ShowNextHitSprite()
     {
         int spriteIndex = timesHit - 1;
-        GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        if (hitSprites[spriteIndex] != null)
+        {
+            GetComponent<SpriteRenderer>().sprite = hitSprites[spriteIndex];
+        }
+        else
+        {
+            Debug.LogError("Block sprite is missing from the array! " + gameObject.name);
+        }
     }
 
     private void HandleHit()
     {
         timesHit++;
+        int maxHits = hitSprites.Length + 1;
         if (timesHit >= maxHits)
         {
             DestroyBlock();
